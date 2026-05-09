fd68aa7026f9f7f84f8589bcbe745f14dd5acf31
mohamed-el3arapy <mohamedel3arapy@gmail.com>
2026-05-09 07:17:20 +0300

    Delete queries/New queries directory

--- queries/New queries/Suggest-query.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    p.name AS Recommended_Product, 
-    p.price, 
-    c.category_name
-FROM Product p
-JOIN Category c ON p.category_id = c.category_id
-WHERE p.category_id = (SELECT category_id FROM Product WHERE product_id = 101)
-  AND p.product_id <> 101
-ORDER BY p.stock_quantity DESC;
